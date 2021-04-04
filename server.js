5576d9aaef9ac833c4e7fb38ae651d6c5fc98e83
Shara Crosslin <sharacrosslin@gmail.com>
2021-04-04 12:44:34 -0700

    removed dotenv for heroku deploy

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 const PORT = 2000;
-require("dotenv").config();
 
 let db,
   dbConnectionStr = process.env.DB_STRING,
